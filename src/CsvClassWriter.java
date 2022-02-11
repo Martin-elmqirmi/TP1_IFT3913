926f3359d22bc511971d2f6d5351649468f15e2d
DaveDarley <46535872+DaveDarley@users.noreply.github.com>
2022-02-10 22:49:21 -0500

    Create CsvClassWriter.java

---
+++ src/CsvClassWriter.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public  class CsvClassWriter{
+
+    /**
+     * Fonction permettant d'entrer les informations necessaires
+     * dans mon fichier "classes.csv"
+     * source:https://www.youtube.com/watch?v=dHZaqMmQNO4
+     * @param nomFichier
+     * @param container
+     */    
+    static void toCsv(String nomFichier, List<String[]> container){
+        String file = nomFichier+".csv";
+        File csvFile = new File(file);
+        try {
+            PrintWriter out = new PrintWriter(csvFile);
+            out.printf("%s, %s, %s, %s, %s\n",
+            "chemin","class","class_LOC","classe_CLOC","classe_DC");
+
+            for(int i = 0; i < container.size(); i++){
+                for(int j = 0; j < container.get(i).length - 1; j++){
+                    out.printf("%s, ",
+                    container.get(i)[j]);
+                }
+                
+                if(i == container.size() - 1){
+                    out.printf("%s",
+                    container.get(i)[container.get(i).length - 1]);
+                }else{
+                    out.printf("%s\n",
+                    container.get(i)[container.get(i).length - 1]);
+                }
+
+            }
+            out.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
