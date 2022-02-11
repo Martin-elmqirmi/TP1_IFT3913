5b24e6ab86328b6b0a36a5ff17c8b7d62f18b81c
DaveDarley <46535872+DaveDarley@users.noreply.github.com>
2022-02-10 22:59:14 -0500

    Update Metrics.java

--- src/Metrics.java
+++ src/Metrics.java
@@ -14,14 +14,15 @@ import java.util.stream.Stream;
 
 public class Metrics {
     
-    // Ces 2 hashmap si on veut stocker les elements pour la partie 2 
+    
     static List <String[]>classInfos = new ArrayList<>();
     static List<String[]> packageInfos = new ArrayList<>();
 
     // contient la liste des dossier de mon chemin
     static ArrayList<String> repositories = new ArrayList<>();
-
-    static Metrics metric ;
+    
+    static CsvPackageWriter csvPackageWriter;
+    static CsvClassWriter csvClassWriter;
 
     /**
      * Fonction recuperant tous les dossiers d'un chemin donne.
@@ -50,8 +51,9 @@ public class Metrics {
 
     /**
      * Fonction recuperant tous les fichiers d'un dossier
-     * specifique, calcul le loc,cloc et dc de chaque fichier.
-     * Stocke les resultat dans un ArrayList.
+     * specifique, calcul le loc,cloc et dc de chaque fichier
+     * se trouvant dans le dossier. Les resultats du calcul sont
+     * ensuite stocké dans mes ArrayList: classInfos et packageInfos
      * @param folder
      */
 
@@ -63,6 +65,8 @@ public class Metrics {
         locPaquet = clocPaquet = wcp = 0;
         double dcPaquet, bcPaquet;
         dcPaquet = bcPaquet = 0;
+        
+        int nbFichier = 0;
 
         File myFolder = new File(folder);
         File[] listOfFiles = myFolder.listFiles(); // list de fichiers dans mon dossier
@@ -73,44 +77,38 @@ public class Metrics {
                 System.out.println(file.getPath());
 
                 String path = file.getPath();
-                
-                // Recuperation du texte a lire
+                if(path.substring(path.length()-5).equals(".java")){
+                    
+                    nbFichier++;
 
-                /*
-                Prob: j'utilise le meme bufferedRead alors qd je 
-                fs une 1ere lecture de mon doc qd je cherche le loc, qd je 
-                vais faire une deuxieme lecture du doc pr le cloc, mon
-                bufferedReader est deja a la fin du document. Et donc line == null
-                alors ca lit rien
-                */
+                    BufferedReader texte1 =  ClassMetrics.readFiles(path);
+                    BufferedReader texte2 =  ClassMetrics.readFiles(path);
+                    BufferedReader texte3 =  ClassMetrics.readFiles(path);
 
-                BufferedReader texte1 =  ClassMetrics.readFiles(path);
-                BufferedReader texte2 =  ClassMetrics.readFiles(path);
-                BufferedReader texte3 =  ClassMetrics.readFiles(path);
+                    // Solution non efficace: creer 3 bufferedReader (a voir comment ameliorer)
 
-                // Solution non efficace: creer 2 bufferedReader (a voir comment ameliorer)
+                    String name = file.getName();
 
-                String name = file.getName();
+                    //test
+                    System.out.println("Le fichier est: "+name);
 
-                //test
-                System.out.println("Le fichier est: "+name);
+                    int loc = ClassMetrics.get_classe_LOC(texte1);
+                    int cloc = ClassMetrics.get_classe_CLOC(texte2);
+                    int wmc = ClassMetrics.get_WMC(texte3);
+                    double dc = ClassMetrics.get_classe_DC(cloc,loc);
+                    double bc = ClassMetrics.get_classe_BC(dc, wmc);
 
-                int loc = ClassMetrics.get_classe_LOC(texte1);
-                int cloc = ClassMetrics.get_classe_CLOC(texte2);
-                int wmc = ClassMetrics.get_WMC(texte3);
-                double dc = ClassMetrics.get_classe_DC(cloc,loc);
-                double bc = ClassMetrics.get_classe_BC(dc, wmc);
-
-                locPaquet += loc;
-                clocPaquet += cloc;
-                wcp += wmc;
-                // infos entrees de la facon qu'il vont etre mis dans le fichier csv
-                classInfos.add(new String[]
-                {path,name,loc+"",cloc+"",dc+"",wmc+"",bc+""});
+                    locPaquet += loc;
+                    clocPaquet += cloc;
+                    wcp += wmc;
+                    // infos entrees de la facon qu'il vont etre mis dans le fichier csv
+                    classInfos.add(new String[]
+                    {path,name,loc+"",cloc+"",dc+"",wmc+"",bc+""});
+                }
             }
         }
-        // si pas de fichier dans le dossier on a pas besoin de calculer ca
-        if(listOfFiles.length != 0){
+        
+        if(nbFichier != 0){ // mon paquet a au moins 1 fichier java
             dcPaquet = (double) clocPaquet/ (double) locPaquet;
             bcPaquet = ClassMetrics.get_paquet_BC(dcPaquet, wcp);
             String name = myFolder.getName();
@@ -119,51 +117,5 @@ public class Metrics {
             {folder,name,locPaquet+"",clocPaquet+"",dcPaquet+"", wcp+"", bcPaquet+""});
         }
     }
-
-
-    // source:https://www.youtube.com/watch?v=dHZaqMmQNO4
-    static void classeCsv(){
-        File csvFile = new File("classes.csv");
-        try {
-            PrintWriter out = new PrintWriter(csvFile);
-            out.printf("%s, %s, %s, %s, %s, %s, %s\n",
-            "chemin","class","class_LOC","classe_CLOC","classe_DC","WMC", "classe_BC");
-
-            for(int i = 0; i < classInfos.size(); i++){
-                for(int j = 0; j < classInfos.get(i).length; j++){
-                    out.printf("%s, ",
-                    classInfos.get(i)[j]);
-                }
-                out.println();
-            }
-            out.close();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-    static void paquetCsv(){
-        File csvFile = new File("paquets.csv");
-        try {
-            PrintWriter out = new PrintWriter(csvFile);
-            out.printf("%s, %s, %s, %s, %s, %s, %s\n",
-            "chemin","paquet","paquet_LOC","paquet_CLOC","paquet_DC","WCP","paquet_BC");
-
-            for(int i = 0; i < packageInfos.size(); i++){
-                for(int j = 0; j < packageInfos.get(i).length; j++){
-                    out.printf("%s, ",
-                    packageInfos.get(i)[j]);
-                }
-                out.println();
-            }
-            out.close();
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
-
-
-    
+  
 }
